483d5b9f9a32f771b3f0351d260955e6c5fbd496
Matheus Vicente <matheusav@outlook.com.br>
2024-05-13 17:31:44 +0200

    Initial Commit

---
+++ UIMaterials/UIMaterialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIMaterialsApp.swift
+//  UIMaterials
+//
+//  Created by Matheus Vicente on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct UIMaterialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
